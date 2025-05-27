4a3d57dd0ec37ef59ae3d6a08fb14c714c728fd8
Nekoru21 <80133990+MoonFoxCake@users.noreply.github.com>
2025-05-27 15:03:29 -0600

    Se añaden mas def y la clase principal

--- Verificador/Verificador.py
+++ Verificador/Verificador.py
@@ -60,43 +60,97 @@ class Visitante:
         self.tabla_simbolos = nueva_tabla_simbolos
     
     
-    def visitar(self, nodo: TipoNodo):
+    def __visitar(self, nodo_actual):
         '''Se usa para visitar los nodos del arbol'''
         '''Hay que poner aca todos los tipos de nodos a visitar'''
 
-    def visitar_asignacion(self, nodo: TipoNodo):
+    def __visitar_asignacion(self,  nodo_actual):
         '''Asignacion es una variable que se le asigna un valor'''
 
-    def visitar_bifurcacion(self, nodo: TipoNodo):
+    def __visitar_bifurcacion(self,  nodo_actual):
         '''Bifurcacion es un if o un else'''
 
-    def visitar_comparacion(self, nodo: TipoNodo):
+    def __visitar_booleano(self,  nodo_actual):
+        '''Booleano es un valor booleano, true o false'''
+
+    def __visitar_bloque_instrucciones(self,  nodo_actual):
+        '''usted que cree hp'''
+
+    def __visitar_booleano(self,  nodo_actual):
+        '''Para Booleanos'''
+
+    
+
+    def __visitar_comparacion(self,  nodo_actual):
         '''Comparacion es una comparacion entre dos valores'''
 
-    def visitar_comparador(self, nodo: TipoNodo):
+    def __visitar_comparador(self,  nodo_actual):
         '''No se que poner aca, pero es un comparador'''
 
-    def visitar_condicion(self, nodo: TipoNodo):
+    def __visitar_condicion(self,  nodo_actual):
         '''Mae ya deberia de saber es la fucken condicion'''
 
-    def visitar_funcion(self, nodo: TipoNodo):
+    def __visitar_entero(self,  nodo_actual):
+        '''Entero es un numero entero'''
+    
+    def __visitar_error(self,  nodo_actual):
+        '''Error, no deberia de llegar aca, pero para poder poner el error por si no se agarra con el analizador, o bueno si se pone quemar'''
+
+    def __visitar_funcion(self,  nodo_actual):
         '''Funcion, def_funcion, tengo que ver que puto desmadre hicieron esos maes con el arbol'''
 
 
-    def visitar_identificador(self, nodo: TipoNodo):
+    def __visitar_identificador(self,  nodo_actual):
         '''Identificador es una variable o una funcion??'''
 
-  
+    def __visitar_matematica(self,  nodo_actual):
+        '''Matematica es una operacion matematica'''
+
+    def __visitar_operador(self,  nodo_actual):
+        '''Operador es un operador matematico, como +, -, *, /'''
+
+    def __visitar_print(self,  nodo_actual):
+        '''Mae para printear'''
 
 
-    def visitar_programa(self, nodo: TipoNodo):
+    def __visitar_programa(self,  nodo_actual):
         '''Programa es el michelin si mal no me acuerdo'''
 
     
 
     
 
-    def visitar_matematica(self, nodo: TipoNodo):
-        '''Matematica es una operacion matematica'''
+class Verificador:
 
-    
+    Arbol: ArbolSintaxisAbstracta
+    visitador: Visitante
+    tabla_simbolos: TablaSimbolos
+
+
+    def __init__(self, nuevoArbol: ArbolSintaxisAbstracta):
+        self.Arbol = nuevoArbol
+        self.tabla_simbolos = TablaSimbolos()
+        self.visitador = Visitante(self.tabla_simbolos)
+
+
+    def print_arbol(self):
+        '''Nada mas recuerden que esta no ocupa ser privada, esta se usara en el main'''
+
+        if self.Arbol.raiz is None:
+            print("Acaso hay arbol mae")
+
+        else:
+            print("Arbol de Sintaxis Abstracta:")
+            self.Arbol.imprimir_preorden(self.Arbol.raiz)
+
+    def __AmbienteEstandar(self):
+        '''Mae aca estan las funciones estandar CREO que son todas, segun el pdf de documentacion, van en minuscula'''
+
+        funciones_estandar = [ ('pelar', TiposDato.FUNCION), ('marinar', TiposDato.FUNCION), ('quemo', TiposDato.ERROR)]
+
+        for Nombre, tipo in funciones_estandar:
+            nodo = Nodo(TiposDato.FUNCION, contendido=Nombre, atributos={'tipo': tipo})
+            self.tabla_simbolos.nuevo_registro(nodo)
+
+    def verificar(self):
+        self.verificar.visitar(self.Arbol.raiz)
