b186e6388ec719233673929127741b57c6da960e
ChepeTEC <jo.gonzalez@estudiantec.cr>
2025-05-29 11:30:54 -0600

    Cambios Verificador
    Implementación de visitar general y como unos 4-5 visitar especificos
    
    Co-Authored-By: Felipe Masís <132018163+fmasis25@users.noreply.github.com>

--- Verificador/Verificador.py
+++ Verificador/Verificador.py
@@ -67,6 +67,106 @@ class Visitante:
         if nodo.tipo is TipoNodo.PROGRAMA:
             self.__visitar_programa(nodo)
 
+        elif nodo.tipo is TipoNodo.ASIGNACION:
+            self.__visitar_asignacion(nodo)
+        
+        elif nodo.tipo is TipoNodo.BIFURCACION:
+            self.__visitar_bifurcacion(nodo)
+
+        elif nodo.tipo is TipoNodo.BLOQUE_INSTRUCCIONES:
+            self.__visitar_bloque_instrucciones(nodo)
+        
+        elif nodo.tipo is TipoNodo.COMPARACION:
+            self.__visitar_comparacion(nodo)
+        
+        elif nodo.tipo is TipoNodo.COMPARADOR:
+            self.__visitar_comparador(nodo)
+        
+        elif nodo.tipo is TipoNodo.CONDICION:
+            self.__visitar_condicion(nodo)
+
+        elif nodo.tipo is TipoNodo.DEF_FUNCION:
+            self.__visitar_funcion(nodo)
+        
+        elif nodo.tipo is TipoNodo.ELIF:
+            self.__visitar_bifurcacion(nodo) #En la función
+        
+        elif nodo.tipo is TipoNodo.ELSE: #No sabemos si hacer uno especifico para else
+            self.__visitar_bifurcacion(nodo)
+        
+        elif nodo.tipo is TipoNodo.ENTERO:
+            self.__visitar_entero(nodo)
+
+        elif nodo.tipo is TipoNodo.ERROR:
+            self.__visitar_error(nodo)
+        
+        elif nodo.tipo is TipoNodo.EXPRESION_MATEMATICA:
+            self.__visitar_expresion_matematica(nodo)
+        
+        elif nodo.tipo is TipoNodo.VARIABLE_MATEMATICA:
+            self.__visitar_matematica(nodo)
+        
+        elif nodo.tipo is TipoNodo.FLOTANTE:
+            self.__visitar_flotante(nodo)
+        
+        elif nodo.tipo is TipoNodo.FUNCION:
+            self.__visitar_funcion(nodo)
+        
+        elif nodo.tipo is TipoNodo.IDENTIFICADOR:
+            self.__visitar_identificador(nodo)
+        
+        elif nodo.tipo is TipoNodo.IF:
+            self.__visitar_bifurcacion(nodo) #No sabemos si hacer uno especifico para if
+
+        elif nodo.tipo is TipoNodo.INSTRUCCION:
+            self.__visitar_instruccion(nodo)
+        
+        elif nodo.tipo is TipoNodo.INVOCACION:
+            self.__visitar_invocacion(nodo)
+        
+        elif nodo.tipo is TipoNodo.OPERADOR:
+            self.__visitar_operador(nodo)
+        
+        elif nodo.tipo is TipoNodo.PALABRA_CLAVE:
+            self.__visitar_palabra_clave(nodo)
+        
+        elif nodo.tipo is TipoNodo.PARAMETROS:
+            self._visitar_parametros (nodo)
+        
+        elif nodo.tipo is TipoNodo.PARA_FUNCION:
+            self._visitar_parametros_funcion(nodo)
+        
+        elif nodo.tipo is TipoNodo.PARA_INVOCACION:
+            self._visitar_parametros_invocacion(nodo)
+        
+        elif nodo.tipo is TipoNodo.PRINCIPAL:
+            self.__visitar_michelin(nodo)
+        
+        elif nodo.tipo is TipoNodo.PRINT:
+            self.__visitar_print(nodo)
+        
+        elif nodo.tipo is TipoNodo.REPETICION:
+            self.__visitar_repeticion(nodo)
+
+        elif nodo.tipo is TipoNodo.RETORNO:
+            self.__visitar_retorno(nodo)
+        
+        elif nodo.tipo is TipoNodo.TEXTO:
+            self.__visitar_texto(nodo)
+        
+        elif nodo.tipo is TipoNodo.AUXILIAR:
+            self.__visitar_auxiliar(nodo)
+
+        elif nodo.tipo is TipoNodo.VALOR_VERDAD:
+            self.__visitar_valor_verdadero(nodo)
+
+        ## elif nodo.tipo is TipoNodo.LITERAL:      No sabemos si ponerlo, ya que no se usa en el arbol
+        ##   self.__visitar_literal(nodo) 
+            
+
+        
+
+
     def __visitar_asignacion(self,  nodo_actual):
         '''Asignacion es una variable que se le asigna un valor'''
         self.tabla_simbolos.nuevo_registro(nodo_actual.nodos[0])
@@ -79,28 +179,56 @@ class Visitante:
         nodo_actual.nodos[0].atributos['tipo']= nodo_actual.nodos[1].atributos['tipo']
 
         
+    def __visitar_auxiliar(self,  nodo_actual):
+
 
     def __visitar_bifurcacion(self,  nodo_actual):
         '''Bifurcacion es un if o un else'''
 
-    def __visitar_booleano(self,  nodo_actual):
-        '''Booleano es un valor booleano, true o false'''
-
     def __visitar_bloque_instrucciones(self,  nodo_actual):
         '''usted que cree hp'''
 
-    def __visitar_booleano(self,  nodo_actual):
-        '''Para Booleanos'''
-
-
     def __visitar_comparacion(self,  nodo_actual):
         '''Comparacion es una comparacion entre dos valores'''
+        for nodo in nodo_actual.nodos:
+            if nodo.tipo == TipoNodo.IDENTIFICADOR:
+                registro = self.tabla_simbolos.verificar_existencia(nodo.contenido)
+            nodo.visitar(self)
+        
+        valor_izquierdo = nodo_actual.nodos[0]
+        comparador = nodo_actual.nodos[1]
+        valor_derecho = nodo_actual.nodos[2]
+
+        if valor_izquierdo.atributos['tipo'] == valor_derecho.atributos['tipo']:
+            comparador.atributos['tipo'] = valor_izquierdo.atributos['tipo']  # Asignamos el tipo de dato del comparador
+
+            nodo_actual.atributos['tipo'] = TiposDato.VALOR_VERDAD  # Asignamos el tipo de dato de la comparacion
+
+        ##PETE CON LO DEL PROFE...
+        ## Caso especial loco: Si alguno de los dos es un identificador de
+        ## un parámetro de función no puedo saber que tipo tiene o va a
+        ## tener por que este lenguaje no es tipado... tons vamos a poner
+        ## que la comparación puede ser cualquiera
+        ##elif valor_izq.atributos['tipo'] == TipoDatos.CUALQUIERA or \
+        ##       valor_der.atributos['tipo'] == TipoDatos.CUALQUIERA:
+        ##
+        ##    comparador.atributos['tipo'] = TipoDatos.CUALQUIERA
+        ##
+        ##    # Todavía no estoy seguro.
+        ##    nodo_actual.atributos['tipo'] = TipoDatos.CUALQUIERA
+
+        else:
+            raise Exception("Los tipos de los valores a comparar no son compatibles", valor_izquierdo.atributos['tipo'], valor_derecho.atributos['tipo'])
 
     def __visitar_comparador(self,  nodo_actual):
         '''No se que poner aca, pero es un comparador'''
 
     def __visitar_condicion(self,  nodo_actual):
         '''Mae ya deberia de saber es la fucken condicion'''
+        for nodo in nodo_actual.nodos:
+            nodo.visitar(self)
+
+        nodo_actual.atributos['tipo'] = TiposDato.BOOLEANO  # Asignamos el tipo de dato de la condicion
 
     def __visitar_entero(self,  nodo_actual):
          """
@@ -111,6 +239,10 @@ class Visitante:
     
     def __visitar_error(self,  nodo_actual):
         '''Error, no deberia de llegar aca, pero para poder poner el error por si no se agarra con el analizador, o bueno si se pone quemar'''
+        for nodo in nodo_actual.nodos:
+            if nodo.tipo == TipoNodo.IDENTIFICADOR:
+                registro = self.tabla_simbolos.verificar_existencia(nodo.contenido)
+            nodo.visitar(self)
 
     def __visitar_expresion_matematica(self,  nodo_actual):
         """
@@ -133,9 +265,19 @@ class Visitante:
     def ___visitar_expresion(self,  nodo_actual):
         '''2 expresiones matematicas con su operador'''
 
+        #for nodo in nodo_actual.nodos:
+
     
     def __visitar_funcion(self,  nodo_actual):
         '''Funcion, def_funcion, tengo que ver que puto desmadre hicieron esos maes con el arbol'''
+        self.tabla_simbolos.nuevo_registro(nodo_actual, TiposDato.FUNCION, nodo_actual.contenido)
+        self.tabla_simbolos.abrir_bloque()  # Abrimos un bloque para la funcion
+        for nodo in nodo_actual.nodos:
+            nodo.visitar(self)
+        
+        self.tabla_simbolos.cerrar_bloque()  # Cerramos el bloque de la funcion
+
+        nodo_actual.atrubutos['tipo'] = nodo_actual.nodos[2].atributos['tipo']  # Asignamos el tipo de dato de retorno de la funcion
 
     def __visitar_flotante(self,  nodo_actual):
         """
@@ -153,34 +295,43 @@ class Visitante:
     def __visitar_instruccion(self,  nodo_actual):
         '''nstruccion ::= (Repeticion | Bifurcacion | Asignacion | Invocacion | Retorno | Error | Comentario )'''
 
-    def __visitar_literal(self,  nodo_actual):
-        '''Literal es un valor literal, o sea un numero, una cadena, un booleano, etc'''
+    ##def __visitar_literal(self,  nodo_actual):
+    ##    '''Literal es un valor literal, o sea un numero, una cadena, un booleano, etc'''
 
     def __visitar_matematica(self,  nodo_actual):
         '''Matematica es una operacion matematica'''
 
     def __visitar_michelin(self,  nodo_actual):
         '''Michelin es el programa principal, o sea el def por asi decirlo playo'''
-
-    def __visitar_numero(self,  nodo_actual):
-        """
-        Numero ::= (Entero | Flotante)
-        """
-
     
     def __visitar_operador(self,  nodo_actual):
         '''Operador es un operador matematico, como +, -, *, /'''
 
+
+    def _visitar_parametros(self,  nodo_actual):
+
     def _visitar_parametros_funcion(self,  nodo_actual):
         '''Parametros de una invocacion, o sea los parametros que se le pasan a una funcion al invocarla'''
+        for nodo in nodo_actual.nodos:
+            self.tabla_simbolos.nuevo_registro(nodo, TiposDato.PARAMETROS, nodo.contenido)
+            nodo.visitar(self)
 
 
     def _visitar_parametros_invocacion(self,  nodo_actual):
         '''Parametros de una invocacion, o sea los parametros que se le pasan a una funcion al invocarla'''
+        for nodo in nodo_actual.nodos:
+            if nodo.tipo == TipoNodo.IDENTIFICADOR:
+                registro = self.tabla_simbolos.verificar_existencia(nodo.contenido)
+            
+            elif nodo.tipo == TipoNodo.FUNCION:
+                raise Exception("Es una funcion, nada que ver con invocacion", nodo.contenido)
+        
+            nodo.visitar(self)
 
     def __visitar_print(self,  nodo_actual):
         '''Mae para printear'''
 
+    def __visitar_palabra_clave(self,  nodo_actual):
 
     def __visitar_programa(self,  nodo_actual):
         '''Programa es el michelin si mal no me acuerdo'''
@@ -194,9 +345,6 @@ class Visitante:
 
         Texto ::= ".*"
         """
-    
-    def __visitar_tipo_componente(self,  nodo_actual):
-        '''Tipo componente es el tipo de dato del componente lexico, o sea el tipo de dato que se esta usando'''
 
     def __visitar_valor_verdadero(self,  nodo_actual):
     
