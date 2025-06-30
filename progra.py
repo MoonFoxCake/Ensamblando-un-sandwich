bcaf1641424a88639220143e8d7272b0c1f02246
Guti2010 <136633660+Guti2010@users.noreply.github.com>
2025-06-30 16:17:40 -0600

    cambio

--- progra.py
+++ progra.py
@@ -8,26 +8,21 @@ def carrera_caracoles(comida, temperatura, humedad):
  print("Condiciones ambientales recibidas")
  while A < meta:
     if B < meta:
-         temp_t = 1
-         t = t + temp_t
+         t = t + 1
          mueveA = 1
          if temperatura > 30:
-                temp_val = 0
-                mueveA = temp_val + 1
+                mueveA = mueveA + 2
          if comida < 5:
-                temp_val2 = 0
-                mueveA = temp_val2 + 1
+                mueveA = mueveA + 0
          if mueveA > 0:
                 A = A + mueveA
                 print("A avanza")
          mueveB = 1
-         if humedad < 20:
-                temp_val4 = 0
-                mueveB = temp_val4 + 1
+         if humedad < 15:
+                mueveB = mueveB + 3
          if temperatura > 20:
                 if temperatura < 25:
-                         temp_val5 = 2
-                         mueveB = temp_val5 + 1
+                         mueveB = mueveB + 3
          if mueveB > 0:
                 B = B + mueveB
                 print("B avanza")
@@ -35,14 +30,12 @@ def carrera_caracoles(comida, temperatura, humedad):
  print("CARRERA TERMINADA")
  gana = 0
  if A > B:
-    temp_val7 = 1
-    gana = temp_val7 + 0
     print("Gana A")
  elif B > A:
-    temp_val8 = 2
-    gana = temp_val8 + 0
     print("Gana B")
  else:
     print("Empate")
  print("Fin")
-carrera_caracoles(10, 25, 25)
+carrera_caracoles(10, 35, 50)
+
+carrera_caracoles(10, 25, 50)
